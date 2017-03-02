1207862e1bbd6c3b359d84bc1c631fcdad7599c3
Sam Light <samlight1994@gmail.com>
2017-03-02 16:45:50 +0000

    Created checkbox field.

--- wp-calc-fields.php
+++ wp-calc-fields.php
@@ -10,9 +10,12 @@
    Text Domain: lscf
  */
 
+$fieldCounter = 0;
+
 function lscf_register_script()
 {
-    wp_register_script('lscf-main-js', plugins_url('js/calcfields.js', __FILE__),
+    wp_register_script('lscf-main-js',
+                       plugins_url('js/calcfields.js', __FILE__),
                        array('jquery'), "0.0.1", true);
 }
 add_action('wp_enqueue_scripts', 'lscf_register_script');
@@ -22,14 +25,18 @@ function lscf_shortcode_init()
     wp_enqueue_script('lscf-main-js');
 }
 
+function lscf_nomalise_id($id)
+{
+    return (strlen($id) >  0 ? "lscf_id_{$id}" : '');
+}
+
 function lscf_sc_total($args, $content)
 {
     $atts = shortcode_atts(array('id' => ''),
                            $args);
-    lscf_shortcode_init();
+    $id = lscf_nomalise_id($atts['id']);
 
-    $id = $atts['id'];
-    if(strlen($id) > 0) $id = "lscf_id_{$id}";
+    lscf_shortcode_init();
 
     $o  = "<span class=\"lscf_total {$id}\">";
     $o .= $content;
@@ -39,10 +46,21 @@ function lscf_sc_total($args, $content)
 }
 add_shortcode('lscf_total', 'lscf_sc_total');
 
-function lsfc_sc_checkbox($args, $content)
+function lscf_sc_checkbox($args, $content)
 {
+    global $fieldCounter;
+
+    $atts = shortcode_atts(array('id' => '',
+                                 'value' => '0'),
+                           $args);
+    $id = lscf_nomalise_id($atts['id']);
+    $hid = 'lscf_cb_' . $fieldCounter++;
+    $val = floatval($atts['value']);
     lscf_shortcode_init();
-    $o = '';
+
+    $o  = "<label for=\"{$hid}\">{$content}</label> ";
+    $o .= "<input type=\"checkbox\" id=\"{$hid}\" class=\"lscf_field {$id}\" " .
+          " value=\"{$val}\" \>";
 
     return $o;
 }
