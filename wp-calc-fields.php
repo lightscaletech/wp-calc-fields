075828e6f64d2db3cabc907bb6e89d54ae8a1570
Sam Light <samlight1994@gmail.com>
2017-03-02 12:01:00 +0000

    Added functions to load scripts and template the shortcodes.

--- wp-calc-fields.php
+++ wp-calc-fields.php
@@ -9,3 +9,33 @@
    License URI: https://www.gnu.org/licenses/gpl-3.0.html
    Text Domain: lscf
  */
+
+function lscf_register_script()
+{
+    wp_register_script('lsfc-main-js', plugins_url('js/calcfields.js'),
+                       array('jquery'), "0.0.1", true);
+}
+add_action('wp_enqueue_scripts', 'lscf_register_script');
+
+function lsfc_shortcode_init()
+{
+    wp_enqueue_script('lsfc-main-js');
+}
+
+function lsfc_sc_total($args, $content)
+{
+    lsfc_shortcode_init();
+    $o = '';
+
+    return $o;
+}
+add_shortcode('lscf_total', 'lscf_sc_total');
+
+function lsfc_sc_checkbox($args, $content)
+{
+    lsfc_shortcode_init();
+    $o = '';
+
+    return $o;
+}
+add_shortcode('lscf_total', 'lscf_sc_checkbox');
