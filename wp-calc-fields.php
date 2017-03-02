9734b237b79750b1642ec2e62c8d770da79036f3
Sam Light <samlight1994@gmail.com>
2017-03-02 11:45:10 +0000

    Created main plugin PHP file.

---
+++ wp-calc-fields.php
@@ -0,0 +1,11 @@
+<?php
+/*
+   Plugin Name: Calculation fields
+   Description: A set of shortcodes to calculate a total on a post
+   Version:     0.0.1
+   Author:      Lightscale Tech Ltd
+   Author URI:  http://lightscale.co.uk/
+   License:     GPL3
+   License URI: https://www.gnu.org/licenses/gpl-3.0.html
+   Text Domain: lscf
+ */
